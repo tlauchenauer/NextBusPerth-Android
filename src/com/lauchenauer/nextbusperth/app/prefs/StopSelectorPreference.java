6f018b8fceeae4dba646714ffd468aa5bf161cb4
Tobias Lauchenauer <tobias@lauchenauer.info>
2012-10-04 16:51:59 +0800

    changed preference to show edit icon

--- src/com/lauchenauer/nextbusperth/app/prefs/StopSelectorPreference.java
+++ src/com/lauchenauer/nextbusperth/app/prefs/StopSelectorPreference.java
@@ -1,12 +1,16 @@
 package com.lauchenauer.nextbusperth.app.prefs;
 
 import android.content.Context;
-import android.preference.Preference;
+import android.preference.DialogPreference;
 import android.util.AttributeSet;
 
-public class StopSelectorPreference extends Preference {
+public class StopSelectorPreference extends DialogPreference {
     public StopSelectorPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
         setPersistent(false);
+    }
+
+    @Override
+    protected void onClick() {
     }
 }
